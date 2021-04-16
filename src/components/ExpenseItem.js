44918914ac549164d8979143238e56f80d3fa888
thenatural86 <philip.smalls86@gmail.com>
2021-04-16 18:42:14 -0400

    state values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,10 +4,11 @@ import './ExpenseItem.css'
 const ExpenseItem = () => {
   const [item, setItem] = useState('Food')
   const [amount, setAmount] = useState('12.99')
+  const date = new Date(2021, 3, 16)
 
   return (
     <div className='expense-item'>
-      <div>April 16th, 2021</div>
+      <div>{date.toDateString()}</div>
       <div className='expense-item__description'>
         <h2>{item}</h2>
         <div className='expense-item__price'>${parseFloat(amount)}</div>
