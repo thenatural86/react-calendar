418f3612f85272636c787bd8505fc6db75a85d39
thenatural86 <philip.smalls86@gmail.com>
2021-04-16 17:44:22 -0400

    prelim styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './ExpenseItem.css'
 
 const ExpenseItem = () => {
-  return <h2>Expense Item</h2>
+  const [item, setItem] = useState('Food')
+  const [amount, setAmount] = useState('12.99')
+
+  return (
+    <div className='expense-item'>
+      <div>April 16th, 2021</div>
+      <div className='expense-item__description'>
+        <h2>{item}</h2>
+        <div className='expense-item__price'>${parseFloat(amount)}</div>
+      </div>
+    </div>
+  )
 }
 
 export default ExpenseItem
