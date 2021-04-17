045d1f8ce6c9ff4e24eb5c8f9e6656501e2af7e1
thenatural86 <philip.smalls86@gmail.com>
2021-04-17 11:37:46 -0400

    date styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import React from 'react'
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = ({ title, amount, date, id }) => {
-  const month = date.toLocaleString('en-US', { month: 'long' })
-  const day = date.toLocaleString('en-US', { day: '2-digit' })
-  const year = date.getFullYear()
-
   return (
-    <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+    <div className='expense-item' id={id}>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
