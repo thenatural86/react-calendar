2807a83c4e44b1889d8a8a79320ea3f00fef48c5
thenatural86 <philip.smalls86@gmail.com>
2021-04-17 10:52:58 -0400

    render multiple components

--- src/App.js
+++ src/App.js
@@ -31,6 +31,24 @@ function App() {
         amount={expenses[0].amount}
         date={expenses[0].date.toDateString()}
       />
+      <ExpenseItem
+        id={expenses[1].id}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date.toDateString()}
+      />
+      <ExpenseItem
+        id={expenses[2].id}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date.toDateString()}
+      />
+      <ExpenseItem
+        id={expenses[3].id}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date.toDateString()}
+      />
     </div>
   )
 }
