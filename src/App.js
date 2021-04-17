c1ed3354fffab215f9c7b5d31c4f234e57373508
thenatural86 <philip.smalls86@gmail.com>
2021-04-17 11:00:17 -0400

    extracting date logic

--- src/App.js
+++ src/App.js
@@ -29,25 +29,25 @@ function App() {
         id={expenses[0].id}
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date.toDateString()}
+        date={expenses[0].date}
       />
       <ExpenseItem
         id={expenses[1].id}
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date.toDateString()}
+        date={expenses[1].date}
       />
       <ExpenseItem
         id={expenses[2].id}
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date.toDateString()}
+        date={expenses[2].date}
       />
       <ExpenseItem
         id={expenses[3].id}
         title={expenses[3].title}
         amount={expenses[3].amount}
-        date={expenses[3].date.toDateString()}
+        date={expenses[3].date}
       />
     </div>
   )
