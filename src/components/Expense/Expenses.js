01b8baadfd6888887e49c23407c0d7219f9f7d16
thenatural86 <philip.smalls86@gmail.com>
2021-04-18 18:29:28 -0400

    add year filter dropdown menu

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,34 +1,51 @@
+import { useState } from 'react'
+import ExpensesFilter from './ExpensesFilter'
 import ExpenseItem from './ExpenseItem'
+
 import './Expenses.css'
 
 const Expenses = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState('2021')
+
+  const yearFilterHandler = (filteredYear) => {
+    setFilteredYear(filteredYear)
+  }
+
+  console.log(filteredYear)
+
   return (
-    <div className='expenses'>
-      <h1>YOLO</h1>
-      <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        id={expenses[3].id}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+    <div>
+      <div className='expenses'>
+        <ExpensesFilter
+          onYearFilter={yearFilterHandler}
+          selected={filteredYear}
+        />
+        <h1>YOLO</h1>
+        <ExpenseItem
+          id={expenses[0].id}
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        />
+        <ExpenseItem
+          id={expenses[1].id}
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        />
+        <ExpenseItem
+          id={expenses[2].id}
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        />
+        <ExpenseItem
+          id={expenses[3].id}
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        />
+      </div>
     </div>
   )
 }
