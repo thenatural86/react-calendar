c121cfeb6f6e61b3fb1300b0c76f9abb7baf7cc7
thenatural86 <philip.smalls86@gmail.com>
2021-04-19 19:00:37 -0400

    render list

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -18,30 +18,18 @@ const Expenses = ({ expenses }) => {
           onYearFilter={yearFilterHandler}
           selected={filteredYear}
         />
-        <ExpenseItem
-          id={expenses[0].id}
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          id={expenses[1].id}
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          id={expenses[2].id}
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
-        <ExpenseItem
-          id={expenses[3].id}
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+
+        {expenses.map((expense) => {
+          console.log(expense)
+          return (
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+              key={expense.id}
+            />
+          )
+        })}
       </div>
     </div>
   )
